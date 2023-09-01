ce9b8bebd5958fab6803edadb3f5935efa9df976
Divine Quansah <divquan@gmail.com>
2023-09-01 05:38:40 +0000

    added: firebase storage handlers

--- firebase/storage.js
+++ firebase/storage.js
@@ -3,7 +3,11 @@ import app from "./config"; // Your Firebase configuration file
 
 const storage = getStorage(firebaseApp);
 
-//this file uploads a file and and return the image link
+/**
+ *this file uploads a file and and return the image link
+ * @param {string} file - the file object to upload
+ *@returns {{string, string}} promise that resolves into {imgUrl, imgPath}
+ */
 export const uploadFile = async (file) => {
   //upload image file
   const filePath = `posts-images/${file.name}`;
@@ -23,14 +27,53 @@ export const uploadFile = async (file) => {
     const imgURL = await getDownloadURL(fileRef);
 
     console.log("Download URL:", imgURL);
-    return { imgURL: downloadURL, img: filePath };
+    return { imgURL: downloadURL, imgPath: filePath };
   } catch (error) {
     console.error("Error getting download URL", error);
     return null;
   }
 };
 
-const updateImage = async (prevImgPath, img) => {
+/*-----------------------------------------------*/
+/*-----------------------------------------------*/
+/*-----------------------------------------------*/
+
+/**
+ * Delete an image from Firebase Storage.
+ * @param {string} imagePath - The path to the image in Firebase Storage.
+ * @returns {Promise} A Promise that resolves when the deletion is complete.
+ */
+export const deleteImage = (imagePath) => {
+  const imageRef = ref(storage, imagePath);
+
+  // Delete the image
+  deleteObject(imageRef)
+    .then(() => {
+      console.log(`Image ${imagePath} deleted successfully`);
+      return {
+        msg: `Image ${imagePath} deleted successfully`,
+        success: true,
+      };
+    })
+    .catch((error) => {
+      console.error(`Error deleting image ${imagePath}:`, error);
+      return { msg: `Image ${imagePath} deleted successfully`, success: false };
+    });
+};
+
+//this function updates the an already saved file with a new one
+//by deleting the old and and updating it with a new one
+/**
+ * Delete an image from Firebase Storage.
+ * @param {string} prevImgPath - The path to the image in Firebase Storage.
+ * @param {File} currentFile - The path to the image in Firebase Storage.
+ *@returns {{string, string}} promise that resolves into {imgUrl, imgPath}
+ */
+export const updateImage = async (prevImgPath, currentFile) => {
   //first delete previous one
+  const deleted = await deleteImage(prevImgPath);
+  if (!deleted) return;
   //upload a new one
+  const updated = await uploadFile(currentFile);
+  return updated;
 };
